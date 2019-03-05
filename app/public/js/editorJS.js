1ce428240b00360daaba074be91ff27d6498d5ba
vinicius <viniciussouzav@gmail.com>
2019-03-05 17:16:39 -0300

    Erro de transporte do conteudo no ENTER, foco errado e <br> criados sozinho

--- app/public/js/editorJS.js
+++ app/public/js/editorJS.js
@@ -1,5 +1,14 @@
-//colocando o foco na linha quando carrega a tela
+//criando linhas ao carregar a página
 $(document).ready(() => {
+    //criando numeração pras linhas
+    for(let i = 2; i <= 100; i++) {
+        $('#linha').append(`<input 
+            id="contador_linha_${i}" 
+            type="button" 
+            class="btn btn-sm btn-dark p-0 m-0" 
+            value="${i}" >`);
+    }
+
     $('#linha_1').text('').focus();
     $('#linha_1').addClass('p-color-back');
     
@@ -10,71 +19,80 @@ $('input').click(function() {
     $(`#linha_${this.defaultValue}`).focus();
 });
 
-//atualizando números das linhas quando apagadas ou crianas no meio
-function atualizaLinhas() {
-    
+function ajustarIdLinha(linhaA, acao) {
+    let ultimoFilho = parseInt($('#editor')[0].childElementCount);
+    let linhaAtual = parseInt(linhaA)
+
+    if(acao == 'insert') {
+        //altrando id das linhas
+        $('#editor p').each( function(item) {
+            $(this).prop('id', `linha_${parseInt(item) + 1}`);
+        });
+
+        
+        /*for(let i = linhaA; i < ultimoFilho; i++)
+            $(`#linha_${i + 1}`).text($(`#linha_${i}`).text()); */
+
+        /*if(linhaTxt != '') {
+            for(let i = linhaA ; i < ultimoFilho ; i++) 
+                $(`#linha_${i}`).text($(`#linha_${i+1}`).text()); 
+        }
+        else
+            console.log('Não tem')*/
+
+        $(`#linha_${linhaA+1}`).focus();
+    }
+    else {
+        if(acao == 'delete') {
+            console.log('aqui');
+            //trocando os conteudos das linhas
+            for(let i = linhaAtual ; i < ultimoFilho ; i++) 
+                $(`#linha_${i}`).text($(`#linha_${i+1}`).text()); 
+
+            $(`#linha_${ultimoFilho}`).remove();
+            $(`#linha_${linhaAtual - 1}`).focus();
+        }
+    }
 }
 
 //caputrando ENTER no p (CRIANDO NOVAS COISAS)
 $('#editor').on('keypress', 'p', function(key) {
+
     if(key.which == 13) {
-        //criando nova linha e incrementadno contador (SECTION LINHA)
-        let linhaAtual = parseInt($(`#contador_linha_${this.id.split('_')[1]}`).val()) + 1;
-       $('#linha').append(`<input 
-            id="contador_linha_${linhaAtual}" 
-            type="button" 
-            class="btn btn-sm btn-dark p-0 m-0" 
-            value="${linhaAtual}" >`);
-        
+        //caputrando linha de ação
+        let linhaAtual = parseInt(this.id.split('_')[1]);
+
         //criando novo p dentro (SECTION EDITPO)
         $('#editor').append(`<p 
-            id="linha_${linhaAtual}" 
-            class="text-break p-color-back p-0 m-0" 
+            id="linha" 
+            class="text-break p-0 m-0 p-color-back" 
             contenteditable="true"></p>`);
 
-        //atualizando numeros das linhas
-        atualizaLinhas();
-
         //dando o foco para a nova linha
-        $(`#linha_${linhaAtual}`).focus();
+        ajustarIdLinha(linhaAtual, 'insert');
     }
 });
 
-//Capturando BACKSPACE no p (DELETANDO AS COISAS)
+//Capturando teclado
 $('#editor').on('keyup', 'p', function(key){
+    //Capturando BACKSPACE no p (DELETANDO AS COISAS)
     if(key.which == 8){
+        //caputrando linha de ação
+        let linhaAtual = parseInt($(`#contador_linha_${this.id.split('_')[1]}`).val());
 
         //vendo se a linhas esta vazia && diferente da primeira linha
-        if(this.textContent == "" && this.id != 'linha_1') {
-
-            //tirando uma linha (SECTION LINHA)
-            let linhaRemove = parseInt($(`#contador_linha_${this.id.split('_')[1]}`).val());
-            
-            $(`#contador_linha_${linhaRemove}`).remove();
-            
-            //romovendo novo p dentro (SECTION EDITPO)
-            $(`#linha_${linhaRemove}`).remove();
-
-            //atualizando numeros das linhas
-            atualizaLinhas();
-    
-            //dando o foco linha de cima
-            $(`#linha_${linhaRemove - 1}`).offset().focus();
-            //$(`#linha_${linhaRemove - 1}`).trigger("keydown", {which: 40});
+        console.log(this.textContent)
+        if(this.textContent == "") {
+            console.log('bora delatea')
+            ajustarIdLinha(linhaAtual, 'delete');
         }
     }
-});
-
-//Capturando setas up e down no p (MOVENDO CURSOR)
-$('#editor').on('keyup', 'p', function(key){
 
     //keyup
     if(key.which == 38){
-
         //verificando se tem linha em cima
         if(this.id != 'linha_1') {
             let linhaAtual = parseInt($(`#contador_linha_${this.id.split('_')[1]}`).val());
-
             $(`#linha_${linhaAtual - 1}`).focus();
         }
     }
@@ -86,3 +104,8 @@ $('#editor').on('keyup', 'p', function(key){
     }
 });
 
+//Capturando setas up e down no p (MOVENDO CURSOR)
+/*$('#editor').on('keyup', 'p', function(key){
+
+    
+});*/
