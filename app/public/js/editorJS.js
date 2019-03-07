08dd71d004b4de05b7ede83871c700e261bd357b
Gustavo Minari <gustavo_minari@hotmail.com.br>
2019-03-07 00:53:05 -0300

    Add load File/Add line count

--- app/public/js/editorJS.js
+++ app/public/js/editorJS.js
@@ -1,22 +1,11 @@
 //Função para criar linhas ao carregar a página
 $(document).ready(() => {
-
-    //deixando input file invisivel
-    $('#arq_uploads').css({opacity: 0});
-
-    //criando numeração pras linhas
-    for(let i = 2; i <= 108; i++) {
-        $('#numeracao').append(`<input 
-            id="contador_linha_${i}" 
-            type="button" 
-            class="btn btn-sm btn-dark p-0 m-0" 
-            value="${i}" >`);
-    }
-
     //trazendo o foco para a primeira linha
     $('#linha').text('').focus();
     $('#linha').addClass('p-color-back');
     $('#linha').append('<br>');
+
+    desenharLinhas();
 });
 
 /*//caputrando ENTER no p (CRIANDO NOVAS COISAS)
@@ -37,28 +26,40 @@ $('#editor').on('keypress', 'p', function(key) {
     }
 });*/
 
-/*
+
+$('#linha').on('input', function() {
+    desenharLinhas();
+});
+
 //Função para abrir arquivo selecionado
 $('input[type=file]').bind('change', function() {
-    let fileName = $(this).val();
+    if(this.files.length != 1) return;
 
-    if(fileName.length > 0) {
-        console.log('here');
-        $.get(fileName, data => console.log('txt'), 'text'); //$('#linha').text(txt)
-    }
-    
-});*/
+    const file = this.files[0];
 
-/*
+    const reader = new FileReader();
+    reader.onload = function(e) {
+        if(e.target.result.indexOf('data:text/plain;base64,') != 0) {
+            console.error("Falha ao ser o arquivo, não é um texto!");
+            return;
+        }
+
+        const textFile = window.atob(e.target.result.replace('data:text/plain;base64,', '').trim());
+        $('#linha').html(text2DivShow(textFile)).focus();
+    };
+    reader.readAsDataURL(file);    
+});
+
+
 //pintar palavras reservadas
-$('#linha').keyup(() => {
-    let palavra = $('#linha').text()
-    if(palavra == 'for') {
-        $('#linha').html('&nbsp <span style="color: black">for</span> &nbsp');
+// $('#linha').keyup(() => {
+//     let palavra = $('#linha').text()
+//     if(palavra == 'for') {
+//         $('#linha').html('&nbsp <span style="color: black">for</span> &nbsp');
 
-        #('#linha').trigger('kreypress', {which:35})
-    }
-});*/
+//         // #('#linha').trigger('kreypress', {which:35})
+//     }
+// });
 
 //ajax de compilador
 $('#compilar').click(() => {
@@ -76,3 +77,38 @@ $('#compilar').click(() => {
         }
     });
 });
+
+
+// Funções para melhorar o Funcionamento
+function desenharLinhas() {
+    const maxLinha = $("#linha > div").length + 1;
+    const linhasDesenhadas = $('#numeracao > input').length;
+    console.log(linhasDesenhadas, maxLinha);
+
+    //criando numeração pras linhas
+    for(let i = linhasDesenhadas; i < maxLinha; i++) {
+        $('#numeracao').append(`<input 
+            id="contador_linha" 
+            type="button" 
+            class="btn btn-sm btn-dark p-0 m-0" 
+            value="${i+1}" >`);
+    }
+
+    // Apaga as Linhas que não existem mais!
+    if(linhasDesenhadas > maxLinha) {
+        for(let i = maxLinha+1; i <= linhasDesenhadas; i++) {
+            $(`#numeracao input#contador_linha[value="${i}"]`).remove();
+        }
+    }
+}
+
+function text2DivShow(textOriginal) {
+    const linhas = textOriginal.replace(/(?:\r\n|\r|\n)/g, '\n').split('\n');
+
+    let textReturn = '';
+    for(i in linhas) {
+        textReturn += '<div>'+linhas[i]+'</div>'
+    }
+
+    return textReturn;
+}
